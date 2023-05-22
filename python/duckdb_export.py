5d8ec6989f1c483140aa6a147d7714f6f5daa62e
Sergey Olontsev <sergey@olontsev.ru>
2023-05-22 07:52:41 +0300

    Schema added for DuckDB conversion.

--- python/duckdb_export.py
+++ python/duckdb_export.py
@@ -3,6 +3,19 @@ import time
 
 if __name__ == '__main__':
     start_time = time.time()
-    rel = duckdb.read_csv('69M_reddit_accounts.csv', header=True, delimiter=',')
+    rel = duckdb.read_csv(
+        '69M_reddit_accounts.csv',
+        header=True,
+        delimiter=',',
+        parallel=True,
+        dtype={
+            'id': 'BIGINT',
+            'name': 'VARCHAR',
+            'created_utc': 'BIGINT',
+            'updated_on': 'BIGINT',
+            'comment_karma': 'BIGINT',
+            'link_karma': 'BIGINT'
+        }
+    )
     rel.write_parquet(file_name='69M_reddit_accounts_duckdb_snappy.parquet', compression='snappy')
     print("Elapsed time: %s seconds" % (time.time() - start_time))
